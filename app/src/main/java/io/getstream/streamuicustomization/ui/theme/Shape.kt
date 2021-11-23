b4eb36087b89cdfbcc68e3cb0e704dbc856414f1
Harun Wangereka <harunwangereka@gmail.com>
2021-11-23 11:38:27 +0300

    Clean up

--- app/src/main/java/io/getstream/streamuicustomization/ui/theme/Shape.kt
+++ app/src/main/java/io/getstream/streamuicustomization/ui/theme/Shape.kt
@@ -1,17 +1,15 @@
 package io.getstream.streamuicustomization.ui.theme
 
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 import io.getstream.chat.android.compose.ui.theme.StreamShapes
 
 val CustomStreamShapes = StreamShapes(
     avatar = RoundedCornerShape(40),
-    myMessageBubble = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp, bottomStart = 16.dp),
-    otherMessageBubble = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp, bottomEnd = 16.dp),
+    myMessageBubble = RoundedCornerShape(topStart = 20.dp, topEnd = 18.dp, bottomStart = 18.dp),
+    otherMessageBubble = RoundedCornerShape(topStart = 18.dp, topEnd = 20.dp, bottomEnd = 18.dp),
     inputField = RoundedCornerShape(24.dp),
     attachment = RoundedCornerShape(16.dp),
-    imageThumbnail = RoundedCornerShape(8.dp),
-    bottomSheet = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+    imageThumbnail = RoundedCornerShape(10.dp),
+    bottomSheet = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
 )
